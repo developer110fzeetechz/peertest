73afbba2ba5eaaa32ee97d79f4f7eea4009d9c12
developer110_Atiq <developer109.fzeetechz@gmail.com>
2024-12-12 17:57:24 +0530

    latest build including cctv and videograpgy functionalities

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+
+    },
+  
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Listen for call request
+  socket.on('call-user', ({ to, offer }) => {
+    console.log(`Call request from ${socket.id} to ${to}`);
+    io.to(to).emit('incoming-call', { from: socket.id, offer });
+  });
+
+  // Listen for answer
+  socket.on('call-accepted', ({ to, answer }) => {
+    console.log(`Call accepted by ${socket.id}`);
+    io.to(to).emit('call-answered', { from: socket.id, answer });
+  });
+
+  // Listen for ICE candidate
+  socket.on('ice-candidate', ({ to, candidate }) => {
+    io.to(to).emit('ice-candidate', { from: socket.id, candidate });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
